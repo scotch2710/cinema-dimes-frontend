deb92b551c5ba04a47270182a43219726a8d88d3
scotch2710 <gallofabrizio98@gmail.com>
2025-06-20 15:49:32 +0200

    2

---
+++ src/app/pages/film-details/film-details.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FilmDetails } from './film-details';
+
+describe('FilmDetails', () => {
+  let component: FilmDetails;
+  let fixture: ComponentFixture<FilmDetails>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [FilmDetails]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(FilmDetails);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/pages/film-details/film-details.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-film-details',
+  imports: [],
+  templateUrl: './film-details.html',
+  styleUrl: './film-details.scss'
+})
+export class FilmDetails {
+
+}
