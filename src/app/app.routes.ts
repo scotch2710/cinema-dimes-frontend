f266fef524312886c74a388179d75c9a5d699c7f
scotch2710 <gallofabrizio98@gmail.com>
2025-06-23 00:19:58 +0200

    home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // Quando l'utente visita l'indirizzo base (es. http://localhost:4200),
+  // mostra il componente HomeComponent.
+  { path: '', component: HomeComponent },
+
+  // Qui in futuro aggiungerai le altre rotte
+  // es. { path: 'film/:id', component: FilmDetailsComponent },
+];
--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
-export class Home {
+export class HomeComponent {
 
 }
